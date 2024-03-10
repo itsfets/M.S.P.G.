8125d03f927065a8ecdc6e675e693afe6911bdff
itsfets <itsfets.outlook.com>
2024-03-10 20:16:59 +0300

    Fixed scaling?

--- android/build/build/generated/source/buildConfig/debug/com/godot/game/BuildConfig.java
+++ android/build/build/generated/source/buildConfig/debug/com/godot/game/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.itsfets.MSPG";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 15;
-  public static final String VERSION_NAME = "1.0.0.15";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "1.0.0.16";
 }
