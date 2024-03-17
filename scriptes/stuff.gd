18b2b8962b3fd3dd4dbd22721426eeac60480c6f
itsfets <itsfets.outlook.com>
2024-03-17 03:56:36 +0300

    Cleaning up

--- scriptes/stuff.gd
+++ scriptes/stuff.gd
@@ -7,12 +7,3 @@ func _new_frame():
 	for x in frame_count:
 		get_node("Area2D/hitbox_"+str(x)).disabled=(x!=C_frame)
 #Thanks to https://forum.godotengine.org/u/ubiguchi/ for this code!
-	print("Hitbox_0 is:",$Area2D/hitbox_0.disabled) 
-	print("Hitbox_1 is:",$Area2D/hitbox_1.disabled) 
-	print("Hitbox_2 is:",$Area2D/hitbox_2.disabled) 
-	print("Hitbox_3 is:",$Area2D/hitbox_3.disabled) 
-	print("Hitbox_4 is:",$Area2D/hitbox_4.disabled) 
-	print("Hitbox_5 is:",$Area2D/hitbox_5.disabled) 
-	print("Hitbox_6 is:",$Area2D/hitbox_6.disabled) 
-	print("Hitbox_7 is:",$Area2D/hitbox_7.disabled) 
-	print()
