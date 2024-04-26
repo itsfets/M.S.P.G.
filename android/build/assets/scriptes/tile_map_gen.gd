9c0e6db25453502e4acf1f488026fe6457ca0a64
itsfets <itsfets.outlook.com>
2024-04-27 00:01:38 +0300

    Updated Godot

--- android/build/assets/scriptes/tile_map_gen.gd
+++ android/build/assets/scriptes/tile_map_gen.gd
@@ -2,8 +2,6 @@ extends Node2D
 @export var noise_text:NoiseTexture2D
 @export var tile_size:Vector2i
 var noise:Noise
-<<<<<<< HEAD
-var array:Array
 var height:int
 var width:int
 func _ready():
@@ -12,25 +10,10 @@ func _ready():
   width=get_viewport_rect().size.x/(scale.x*tile_size.x)+1
   height=get_viewport_rect().size.y/(scale.y*tile_size.y)+1
   _generate()
-=======
-var height:int
-var width:int
-
-func _ready():
-  noise_text.noise.seed=randi()
-  noise=noise_text.noise
-  width=get_viewport_rect().size.x/(4*tile_size.x)+1
-  height=get_viewport_rect().size.y/(4*tile_size.y)+1
-  _generate()
-  print(width,height)
-
->>>>>>> 69d6e8c7964ff2c4c779434cf13cf95640042b17
 func _generate():
   for x in range(width):
     for y in range(height):
       var noise_val:float = noise.get_noise_2d(x,y)
-<<<<<<< HEAD
-      array.append(noise_val)
       if noise_val<-0.3:
         $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(0,0),randi_range(0,3))
       elif noise_val<-0.2:
@@ -47,23 +30,3 @@ func _generate():
         $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(2,1),randi_range(0,3))
       else:
         $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(3,1),randi_range(0,3))
-  print(array.min())
-  print(array.max())
-=======
-      if noise_val<-0.6:
-        $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(0,0),0)
-      elif noise_val<-0.4:
-        $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(1,0),0)
-      elif noise_val<-0.2:
-        $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(2,0),0)
-      elif noise_val<0:
-        $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(3,0),0)
-      elif noise_val<0.2:
-        $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(0,1),0)
-      elif noise_val<0.4:
-        $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(1,1),0)
-      elif noise_val<0.6:
-        $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(2,1),0)
-      else:
-        $TileMap.set_cell(0,Vector2i(x,y),0,Vector2i(3,1),0)
->>>>>>> 69d6e8c7964ff2c4c779434cf13cf95640042b17
--- android/build/build/intermediates/assets/debug/mergeDebugAssets/scriptes/stuff.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-var hitbox:PackedVector2Array
-var C_frame=0
-@export var frame_count=8
-func _new_frame():
-  C_frame=$Area2D/AnimatedSprite2D.get_frame()
-  for x in frame_count:
-    get_node("Area2D/hitbox_"+str(x)).disabled=(x!=C_frame)
-#Thanks to https://forum.godotengine.org/u/ubiguchi/ for this code!
